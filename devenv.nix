14a7ad4e5f9220be0fda43f143bb6cc98d108257
Nico Zamora <dev@zico.xyz>
2024-06-24 11:10:57 -0400

    feat(devenv): add aws-cli package

--- devenv.nix
+++ devenv.nix
@@ -7,9 +7,10 @@
 }: {
   env.GREET = "Initializing rune-pouch";
   cachix.enable = false;
-  packages = with pkgs; [git corepack nodejs-slim jq];
+  packages = with pkgs; [git corepack nodejs-slim jq awscli2];
   languages = {
     typescript.enable = true;
+    python.enable = true;
   };
 
   enterShell = ''
