78152f1aaf029fc97bf8638ce47ebae8afa2abb5
Nico Zamora <dev@zico.xyz>
2024-06-24 12:48:49 -0400

    feat(logging): added git-chglog package

--- devenv.nix
+++ devenv.nix
@@ -7,7 +7,7 @@
 }: {
   env.GREET = "Initializing rune-pouch";
   cachix.enable = false;
-  packages = with pkgs; [git corepack nodejs-slim jq awscli2];
+  packages = with pkgs; [git corepack nodejs-slim jq awscli2 git-chglog];
   languages = {
     typescript.enable = true;
     python.enable = true;
