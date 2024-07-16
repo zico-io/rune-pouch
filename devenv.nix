3d3bd7cdd3453accef207e95141822df8e64789f
Nico Zamora <dev@zico.xyz>
2024-07-16 12:20:26 -0400

    chore(devenv): include ts-server

--- devenv.nix
+++ devenv.nix
@@ -7,7 +7,7 @@
 }: {
   env.GREET = "Initializing rune-pouch";
   cachix.enable = false;
-  packages = with pkgs; [git corepack nodejs-slim jq awscli2 git-chglog];
+  packages = with pkgs; [git corepack nodejs-slim jq awscli2 git-chglog nodePackages.typescript-language-server];
   languages = {
     typescript.enable = true;
     python.enable = true;
