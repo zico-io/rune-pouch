49e51cd9dc00edfb6616a63f8691f0420f1be15a
Nico Zamora <dev@zico.xyz>
2024-07-21 14:28:31 -0400

    feat(nav): Highlight active NavButton

--- src/components/ui/NavButton.tsx
+++ src/components/ui/NavButton.tsx
@@ -3,19 +3,23 @@ import { Link } from "@tanstack/react-router";
 
 export const NavButton = ({ href, icon }: { href: string; icon: string }) => {
   const link = `https://oldschool.runescape.wiki/images/${icon}.png`
+
   return (
     <Link to={href}>
-      <Paper elevation={2}>
-        <IconButton
-          sx={{
-            width: 48,
-            height: 48,
-            borderRadius: 1,
-          }}
-        >
-          <img src={link} />
-        </IconButton>
-      </Paper>
+      {({ isActive }) => (
+        <Paper elevation={isActive ? 24 : 2}>
+          <IconButton
+            sx={{
+              width: 48,
+              height: 48,
+              borderRadius: 1,
+            }}
+          >
+            <img src={link} />
+          </IconButton>
+        </Paper>
+
+      )}
     </Link>
   )
 }
