c3c7285e7212adb3a3ba4f1da7d344d4bbcb2554
Nico Zamora <dev@zico.xyz>
2024-07-21 23:12:29 -0400

    fix(dps): Moved Container to page level

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,7 @@
-import { Container } from '@mui/material'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
   component: () => (
-    <Container fixed sx={{ height: '100vh', overflowY: 'scroll', overflowX: 'hidden', scrollbarWidth: 'none' }}>
-      <Outlet />
-    </Container>
+    <Outlet />
   ),
 })
